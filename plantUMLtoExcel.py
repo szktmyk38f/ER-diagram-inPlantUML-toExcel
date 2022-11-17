b7863704216b549a8d8074feb9e22cb5ca07bee9
Tomoyuki <season2864@gmail.com>
2022-11-18 00:21:06 +0900

    feat: Multiple support, additional ruled lines

--- plantUMLtoExcel.py
+++ plantUMLtoExcel.py
@@ -1,7 +1,10 @@
 import re
 import openpyxl
 from openpyxl.utils import get_column_letter
+from openpyxl.styles.borders import Border, Side
 
+side = Side(style='thin', color='000000')
+border = Border(top=side, bottom=side, left=side, right=side)
 
 wb = openpyxl.Workbook()
 ws1 = wb.active
@@ -14,24 +17,32 @@ lines = test_data.readlines()
 height = 0
 is_first = True
 seq = 1
+start_ruled_line = 0
+end_ruled_line = 0
 
 for line in lines:
     # create header
-    if is_first:
-        ws1['C7'] = '#'
-        ws1['D7'] = 'PK'
-        ws1['E7'] = 'FK'
-        ws1['F7'] = 'column name'
-        ws1['G7'] = 'item name'
-        ws1['H7'] = 'type'
-        ws1['I7'] = 'length'
-        ws1['J7'] = 'not null'
-        ws1['K7'] = 'default'
+    if is_first and '{' in line:
+        ws1['C' + str(7 + height)] = '#'
+        ws1['D' + str(7 + height)] = 'PK'
+        ws1['E' + str(7 + height)] = 'FK'
+        ws1['F' + str(7 + height)] = 'column name'
+        ws1['G' + str(7 + height)] = 'item name'
+        ws1['H' + str(7 + height)] = 'type'
+        ws1['I' + str(7 + height)] = 'length'
+        ws1['J' + str(7 + height)] = 'not null'
+        ws1['K' + str(7 + height)] = 'default'
         is_first = False
 
     if '}' in line:
-        height += 3
+        end_ruled_line = height
+        for row in ws1['C' + str(7 + start_ruled_line) + ':K' + str(8 + end_ruled_line)]:
+            for cell in row:
+                cell.border = border
+        height += 7
+        start_ruled_line = height
         is_first = True
+        seq = 1
         continue
 
     if '--' in line or '@startuml' in line or '@enduml' in line:
@@ -44,25 +55,25 @@ for line in lines:
     if entity_index != -1:
         table_physical_name = re.search(r'entity "(.+)" ', line).group(1)
         table_logical_name = re.search(r'as (.+) {', line).group(1)
-        ws1['A3'] = "physical name"
-        ws1['A4'] = table_physical_name
-        ws1['B3'] = "logical name"
-        ws1['B4'] = table_logical_name
+        ws1['A' + str(3 + height)] = "physical name"
+        ws1['A' + str(4 + height)] = table_physical_name
+        ws1['B' + str(3 + height)] = "logical name"
+        ws1['B' + str(4 + height)] = table_logical_name
     elif primary_index != -1:
-        ws1['C8'] = seq
+        ws1['C' + str(8 + height)] = seq
         seq += 1
         if '+' in line:
             primary_key_name = re.search(r'\+ (.+) ', line).group(1)
-            ws1['D8'] = '○'
-            ws1['F8'] = primary_key_name
+            ws1['D' + str(8 + height)] = '○'
+            ws1['F' + str(8 + height)] = primary_key_name
         elif '*' in line:
             primary_key_name = re.search(r'\* (.+) ', line).group(1)
-            ws1['D8'] = '○'
-            ws1['F8'] = primary_key_name
+            ws1['D' + str(8 + height)] = '○'
+            ws1['F' + str(8 + height)] = primary_key_name
         else:
             primary_key_name = re.search(r'        (.+) ', line).group(1)
-            ws1['D8'] = '○'
-            ws1['F8'] = primary_key_name
+            ws1['D' + str(8 + height)] = '○'
+            ws1['F' + str(8 + height)] = primary_key_name
     elif foreign_index != -1:
         ws1['C' + str(9 + height)] = seq
         seq += 1
@@ -76,7 +87,6 @@ for line in lines:
             ws1['F' + str(9 + height)] = foreign_key_name
         height += 1
     else:
-        print(line)
         line = line.strip()
         if len(line) != 0:
             ws1['C' + str(9 + height)] = seq
